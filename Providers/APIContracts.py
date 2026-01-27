9dc057c4dd5f6dfa909d4285ab6191436eb81dc7
Nadil Karunarathna <nkan0027@student.monash.edu>
2026-01-27 18:44:55 +1100

    Edit some things

--- Providers/APIContracts.py
+++ Providers/APIContracts.py
@@ -9,7 +9,34 @@ class ChatMessageStructure(BaseModel):
     context: str
     userQ: str
     
+class PaymentStructure(BaseModel):
+    site_id: Optional[str]
+    status: Optional[str]
+    trial_end: Optional[str]
+    current_end: Optional[str]
 
+class ChatBotEdits(BaseModel):
+    site_id: str
+    chabot_name: Optional[str]
+    personality: Optional[str]
+    tone: Optional[str]
+    resp_length: Optional[str]
+    temperature: Optional[float]
+    greeting: Optional[str]
+    fallback: Optional[str]
+    updated_at: Optional[str] = None
+
+class WidgetAppearance(BaseModel):
+    site_id: str
+    widget_color: Optional[str]
+    widget_size: Optional[str]
+    border_radius: Optional[int]
+    updated_at: Optional[str] = None
+
+class AvatarStructure(BaseModel):
+    site_id: str
+    AvatarUrl: Optional[str]
+    updated_at: str
 
 class ChatResponse(BaseModel):
     site_id: str
