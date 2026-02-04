52dd7ed973bfe7b857409dbf2996a180898381a0
Nadil Karunarathna <nkan0027@student.monash.edu>
2026-02-04 12:46:58 +1100

    debug testing (temp)

--- Router/startup_router.py
+++ Router/startup_router.py
@@ -30,15 +30,19 @@ def create_client(details: ClientListSetUp):
 def get_siteid_wo_client(firebase_id: str = Query(...)):
     return rag.get_siteid_wo_client(firebase_id)
 
+# @router.get("/get_widget_information")
+# def get_widget_information(site_id: str, request: Request):
+#     domain = startup.get_request_domain(request)
+#     if not domain:
+#         raise HTTPException(status_code=403, detail="Missing Origin/Referer (browser did not send)")
+
+#     domain = domain.lower().removeprefix("www.").strip()
+
+#     if not rag.domain_allowed(site_id, domain):
+#         raise HTTPException(status_code=403, detail="Domain not allowed")
+
+#     return rag.get_appearence(SiteID(site_id=site_id))
+
 @router.get("/get_widget_information")
-def get_widget_information(site_id: str, request: Request):
-    domain = startup.get_request_domain(request)
-    if not domain:
-        raise HTTPException(status_code=403, detail="Missing Origin/Referer (browser did not send)")
-
-    domain = domain.lower().removeprefix("www.").strip()
-
-    if not rag.domain_allowed(site_id, domain):
-        raise HTTPException(status_code=403, detail="Domain not allowed")
-
+def get_widget_information(site_id: str):
     return rag.get_appearence(SiteID(site_id=site_id))
