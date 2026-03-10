aed6eb1dc4fe4f5bb6317025cba39a74e2781591
Nadil Karunarathna <nkan0027@student.monash.edu>
2026-03-10 11:34:40 +1100

    Initial Commit

--- Providers/APIContracts.py
+++ Providers/APIContracts.py
@@ -6,8 +6,8 @@ from datetime import datetime
 class ChatRequest(BaseModel):
     site_id: str
     message: str
-    pastMessages: List[str] 
-    pastAnswers: List[str] 
+    pastMessages: List[str] = None
+    pastAnswers: List[str] = None
 
 class ChatMessageStructure(BaseModel):   
     context: str
@@ -15,24 +15,24 @@ class ChatMessageStructure(BaseModel):
 
 class ChatBotEdits(BaseModel):
     user_id: str
-    name: Optional[str]
-    email: Optional[str]
-    phone: Optional[str]
-    monthly_token_limit = Optional[str]
-    monthly_token_used = Optional[str]  
+    name: Optional[str] = None 
+    email: Optional[str] = None
+    phone: Optional[str] = None
+    monthly_token_limit = Optional[str] = None
+    monthly_token_used = Optional[str] = None 
 
 class AccountInit(BaseModel):
     user_id: str
-    name: Optional[str]
-    email: Optional[str]
-    phone: Optional[str]
-    created_at = Optional[str]
-    subscription_status = Optional[str]
-    stripe_customer_id = Optional[str]
-    stripe_subscription_id = Optional[str]
-    monthly_token_limit = Optional[str]
-    monthly_token_used = Optional[str]  
-    billing_cycle_start = Optional[str]
+    name: Optional[str] = None
+    email: Optional[str] = None
+    phone: Optional[str] = None
+    created_at = Optional[str]= None
+    subscription_status = Optional[str] = None
+    stripe_customer_id = Optional[str] = None
+    stripe_subscription_id = Optional[str] = None
+    monthly_token_limit = Optional[str] = None
+    monthly_token_used = Optional[str] = None
+    billing_cycle_start = Optional[str] = None
 
 class SessionInit(BaseModel):
     userID: str
@@ -41,13 +41,13 @@ class SessionInit(BaseModel):
 class SessionCreate(BaseModel):
     id: str
     user_id: str
-    created_at: Optional[str]
-    rive_avatar: Optional[str]
-    last_message: Optional[str]
-    welcome_message: Optional[str]
+    created_at: Optional[str] = None
+    rive_avatar: Optional[str] = None
+    last_message: Optional[str] = None
+    welcome_message: Optional[str] = None
     status: Optional[str] = 'Open'
-    summary: Optional[str]
-    title: Optional[str]
+    summary: Optional[str] = None
+    title: Optional[str] = None
 
 
 
@@ -66,11 +66,11 @@ class VoiceChat(BaseModel):
 
     
 class ClientListSetUp(BaseModel):
-    name: Optional[str]
-    email: Optional[str]
-    phone: Optional[str]
-    address: Optional[str]
-    country: Optional[str]
+    name: Optional[str] = None
+    email: Optional[str] = None
+    phone: Optional[str] = None
+    address: Optional[str] = None
+    country: Optional[str] = None
     subscription: str
     account_id: str
     subscription_end: datetime | None = None
