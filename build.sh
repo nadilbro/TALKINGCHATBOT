565ff3377f075007b4da77996a81910cb12f6cf3
Nadil Karunarathna <nkan0027@student.monash.edu>
2026-03-18 19:04:02 +1100

    Febuggnig 7

--- build.sh
+++ build.sh
@@ -3,17 +3,12 @@ set -e
 
 pip install -r requirements.txt
 
-# Install espeak statically (no apt needed)
-wget -q https://github.com/espeak-ng/espeak-ng/releases/download/1.51/espeak-ng-1.51-linux-x64.zip
-unzip -q espeak-ng-1.51-linux-x64.zip
-cp espeak-ng ./espeak-ng
-chmod +x ./espeak-ng
+# Just check what's available
+which espeak || echo "espeak not found"
+which espeak-ng || echo "espeak-ng not found"
 
-# Install Rhubarb
+# Rhubarb
 wget -q https://your-url/rhubarb-lip-sync-1.13.0-linux.zip
 unzip -q rhubarb-lip-sync-1.13.0-linux.zip
 cp Rhubarb-Lip-Sync-1.13.0-Linux/rhubarb ./rhubarb
 chmod +x ./rhubarb
-
-echo "==> Done"
-./rhubarb --version
