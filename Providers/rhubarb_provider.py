356bd09a846c1325f168323dcf99a88c3bf832ab
Nadil Karunarathna <nkan0027@student.monash.edu>
2026-03-18 21:01:07 +1100

    debugging

--- Providers/rhubarb_provider.py
+++ Providers/rhubarb_provider.py
@@ -82,7 +82,7 @@ class RhubarbProvider:
             text_f.close()
             tmp_files.append(text_f.name)
 
-            cmd = [self.rhubarb_bin, "-f", "json", "--recognizer", "pocketSphinx"]
+            cmd = [self.rhubarb_bin, "-f", "json", "--recognizer", "phonetic"]
 
             if audio_bytes:
                 audio_f = tempfile.NamedTemporaryFile(
