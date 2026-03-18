d2edf2670b5400109ea20fd98e415941dc1656f4
Nadil Karunarathna <nkan0027@student.monash.edu>
2026-03-18 18:34:52 +1100

    Febuggnig 2

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ pip install -r requirements.txt
 wget -q https://github.com/DanielSWolf/rhubarb-lip-sync/releases/download/v1.13.0/rhubarb-lip-sync-1.13.0-linux.zip
 unzip -q rhubarb-lip-sync-1.13.0-linux.zip
 
-# Show us exactly what got extracted
-echo "==> Extracted contents:"
-find . -name "rhubarb*" -type f
+mv Rhubarb-Lip-Sync-1.13.0-Linux/rhubarb /usr/local/bin/rhubarb
+chmod +x /usr/local/bin/rhubarb
+
+echo "==> Rhubarb installed at:"
+which rhubarb
+rhubarb --version
