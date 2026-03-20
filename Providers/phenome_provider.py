8ae53529f279ec82473b9737a19a615aa84d037a
Nadil Karunarathna <nkan0027@student.monash.edu>
2026-03-20 12:58:06 +1100

    trying to addd stremaing: Debugging

--- Providers/phenome_provider.py
+++ Providers/phenome_provider.py
@@ -2,8 +2,7 @@ import re
 from typing import List, Dict, Any
 
 import nltk
-# Download CMU dict if not already present
-nltk.download("cmudict", quiet=True)
+
 
 # ---------------------------------------------------------------------------
 # ARPAbet phoneme → Preston Blair viseme ID (0–8)
