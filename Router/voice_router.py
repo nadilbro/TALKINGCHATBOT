1adcad036af089675db64ddd02870bc9d6ffd931
Nadil Karunarathna <nkan0027@student.monash.edu>
2026-04-10 11:54:04 +1000

    Bomboclat

--- Router/voice_router.py
+++ Router/voice_router.py
@@ -617,6 +617,8 @@ async def audio_chat_ws(ws: WebSocket):
             # ----------------------------------------------------------
             # TTS (only if audio_on AND text is not too long)
             # ----------------------------------------------------------
+            print(f"MARKDOWN TEXT {bot_text}")
+            print(f"CLEANED TEXT {cleaned_sentences}")
             if audio_on:
                 try:
                     cleaned_sentences = [strip_markdown(s) for s in sentences]
